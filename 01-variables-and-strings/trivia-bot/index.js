faf72417bbac686a98498d16180ef9ea81ce3ebc
divkokal <divya.skokal@gmail.com>
2026-03-25 22:50:09 +0000

    Add trivia bot

---
+++ 01-variables-and-strings/trivia-bot/index.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "botRobot";
+const botLocation = "Mars";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact = "I love running infinite loops in " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "I shouldn't love running infinite loops in " + favoriteLanguage;
+
+console.log(codingFact)
+
+codingFact = "I love when my laptop shuts down from infinite loops in " + favoriteLanguage;
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
