9f1a96716c5593f95b9939d0ffd3ec991057df4b
divkokal <divya.skokal@gmail.com>
2026-03-26 21:40:32 +0000

    Add sentence-maker

---
+++ 01-variables-and-strings/sentence-maker/index.js
@@ -0,0 +1,21 @@
+let adjective = "big";
+let noun = "dragon";
+let verb = "flying";
+let place = "park";
+let adjective2 = "ginormous";
+let noun2 = "people";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "beautiful";
+noun = "people";
+verb = "cooking";
+place = "restaurant";
+adjective2 = "big";
+noun2 = "chef";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory)
